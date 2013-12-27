2c69d93493812731efcbe5625492f6483eb64f5b
Thiago Nascimento <thiagodnf@gmail.com>
2013-12-27 09:07:40 -0300

    Retirado o código javascript dos componentes sociais e colocado em arquivos js separados

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,12 +1,24 @@
 // Author: Thiago Nascimento
 // Filename: app.js
 
-define(['jquery','underscore','backbone','router','bootstrap','spin','jqueryspin'], function($, _, Backbone, Router,Bootstrap){
+define([
+  'jquery',
+  'underscore',
+  'backbone',
+  'router',
+  'bootstrap',
+  'spin',
+  'jqueryspin',
+  'util'
+], function($, _, Backbone, Router,Bootstrap,Spin,jQSpin,u){
 
   //Key to Google Fusion Table
   window.key = "AIzaSyBD5rcWj2_fuugaOICorW1fVOrEFSErO4g"
 
   var initialize = function(){
+
+    u.social()
+
     //All request by Google Fusion Table
     $.ajaxPrefilter( function( options, originalOptions, jqXHR ) {
       options.url = 'https://www.googleapis.com/fusiontables/v1/' + options.url;
